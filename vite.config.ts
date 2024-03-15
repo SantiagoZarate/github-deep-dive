f90a559d4ad12fe2dee25d6ef446cc0404b68d8b
Santiago Zarate <santiago2001zarate@gmail.com>
2024-03-15 01:11:42 -0300

    chore: add alias for hooks, components, utils and icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "happy-dom",
+  },
+  resolve: {
+    alias: {
+      "@component": path.resolve(__dirname, "src/components"),
+      "@icon": path.resolve(__dirname, "src/components/icons/index.tsx"),
+      "@hook": path.resolve(__dirname, "src/hooks"),
+      "@util": path.resolve(__dirname, "src/utils"),
+    },
   },
 });
