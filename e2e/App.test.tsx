a72e56a444af9fc3258e59f4c3e01aae2a8c5d80
Santiago Zarate <santiago2001zarate@gmail.com>
2024-03-15 01:05:45 -0300

    chore: add test dependencies

---
+++ e2e/App.test.tsx
@@ -0,0 +1,3 @@
+// import { expect, describe, should, test, beforeEach, afterEach } from "vitest";
+// import { screen, render, cleanup } from "@testing-library/react";
+// import { UserEvent } from "@testing-library/user-event";
