c55d6061321974034292b6822ab4fe15de316585
Santiago Zarate <santiago2001zarate@gmail.com>
2024-03-15 01:48:00 -0300

    test: add trivial test

--- e2e/App.test.tsx
+++ e2e/App.test.tsx
@@ -1,3 +1,9 @@
-// import { expect, describe, should, test, beforeEach, afterEach } from "vitest";
+import { expect, describe, test } from "vitest";
 // import { screen, render, cleanup } from "@testing-library/react";
 // import { UserEvent } from "@testing-library/user-event";
+
+describe("trivial test", () => {
+  test("2 should be 2", () => {
+    expect(2).toBe(2);
+  });
+});
